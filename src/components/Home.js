d37daec11cf4c35e21f307a7af44e9d3b87fdd5e
nathallyet <nathallyet@gmail.com>
2022-07-16 15:55:42 -0300

    Passagem de dados via props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 const Home = (props) => {
+  console.log(props);
   return (
     <>
+      <h3>{props.tituloPagina}</h3>
+      {/* Para acessar as constantes diretas de dentro de props */}
+      {/* 
+      <p>
+        ISBN: {props.isbn}<br/>
+        Título: {props.titulo}<br/>
+        Autor: {props.autor} 
+      </p> 
+      */}
+      {/* Para acessar um array de objetos de dentro de props(usando o método map para percorrer o array em cada um dos seus indices(i) e em seguida usar o ponto(.) para acessar a propriedade em questão) */}
+      {props.dados.map((item, i) => (
+        <p key={i}>
+          ISBN: {props.dados[i].isbn}<br/>
+          Título: {props.dados[i].titulo}<br/>
+          Autor: {props.dados[i].autor}
+        </p>
+      ))}
+      {props.children}
+
+      {/* Antes de usar o recebimento de dados via props */}
+      {/* 
       <h2>Últimos lançamentos</h2>
       <div className="card">
         <div className="thumb">
@@ -23,7 +45,8 @@ const Home = (props) => {
           <p>Introdução ao Pentest...</p>
           <a href="#">Leia mais &gt;</a>
         </div>
-      </div>
+      </div> 
+      */}
     </>
   );
 };
--- src/components/Principal.js
+++ src/components/Principal.js
@@ -3,9 +3,28 @@ import React from "react";
 import Home from "./Home";
 
 const Principal = (props) => {
+
+  const tituloPagina = "Últimos lançamentos";
+  // const isbn = "978-85-7522-xxx-x";
+  // const titulo = "React";
+  // const autor = "Maujor";
+  const dados = [
+    { isbn: "978-85-75-22-xxx-x", titulo: "React", autor: "Maujor" },
+    { isbn: "978-85-75-22-776-3", titulo: "Design UX", autor: "Will" },
+    { isbn: "978-85-75-22-550-9", titulo: "Laravel para Ninjas", autor: "Jack" }
+  ]
+
   return (
     <main className="principal">
-      <Home />
+      <Home
+        tituloPagina={tituloPagina}
+        // isbn={isbn}
+        // titudo={titulo}
+        // autor={autor} 
+        dados={dados}
+        >
+        <h3>Esse conteúdo é acessado através de props.children</h3>
+      </Home>
     </main>
   );
 };
